ad7c7be1a06bb1544d4db4bf80c0a41d5583b3ad
Taieb OUBERKA <ouberka021@gmail.com>
2022-06-29 12:08:57 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// Your Ship
+// Battle Ship Game
+
 class USS_Schwarzenegger {
   constructor() {
-    this.hull = 20;
+    this.hull = 15;
     this.firepower = 5;
     this.accuracy = .7
   }
@@ -10,18 +11,17 @@ class USS_Schwarzenegger {
     let rand = Math.random()
     if (rand < this.accuracy) {
       invader.hull -= this.firepower;
-      alert(`YOUR ATTACK WAS SUCCESSFUL! \n\n PREPARE FOR INCOMING ATTACK...`)
+      alert(`Attack Your Goals Was SUCCESSFUL! \n\n Be Prepare for Mass Attacks.`)
     } else {
-      alert(`YOUR ATTACK FAILED! \n\n PREPARE FOR INCOMING ATTACK...`)
+      alert(`YOUR ATTACK FAILED!.`)
     }
   }
 }
-
 const defender = new USS_Schwarzenegger()
 
 
 // Alien Ship
-class Alien {
+class AlienShip {
   constructor() {
     this.hull = Math.floor(Math.random() * (7 - 3) + 3)
     this.firepower = Math.floor(Math.random() * (5 - 2) + 2)
@@ -38,3 +38,72 @@ class Alien {
   }
 }
 
+const invader = new Alien()
+
+
+// Initiate Game
+const start = () => {
+  alert('Welcome to Battling Alien Spaceships!');
+  const response = prompt(`Enter 'yes' to play or 'no' to exit.`);
+  if (response.toLowerCase() === 'no') {
+    alert(`Good-bye!`)
+  } else if (response.toLowerCase() === 'yes') {
+    alert('You are the captain, you have to protect the earth from spaceships.\n\nYour mission is to destroy every last alien ship.')
+    lockAndLoad()
+  } else {
+    alert(`Try again next time!\n\n yes or no`)
+  }
+}
+
+
+// Check Status
+const status = () => {
+  alert(`Your Status ----> HULL: ${defender.hull} \n\n FIREPOWER: ${defender.firepower} \n\n ACCURACY: ${defender.accuracy*100}%\n\nAlien#${invaderCount} Status >>> HULL: ${invader.hull} | FIREPOWER: ${invader.firepower} | ACCURACY: ${Math.floor(invader.accuracy*100)}%`)
+}
+
+
+// Get Ready to Battle
+invaderCount = 6;
+
+const lockAndLoad = () => {
+
+  status()
+
+  while (invaderCount > 0) {
+    if (invader.hull < 1) {
+      invaderCount--
+    }
+
+    if (invaderCount > 0 && defender.hull > 0) {
+      invader.hull = Math.floor(Math.random() * (7 - 3) + 3)
+      invader.firepower = Math.floor(Math.random() * (5 - 2) + 2)
+      invader.accuracy = Math.random() * (.8 - .6) + .6
+    } else if (invaderCount < 1 && defender.hull < 1) {
+      return alert(`TIE GAME!`)
+    } else if (invaderCount < 1 && defender.hull > 0) {
+      return alert(`GOOD JOB, YOU HAVE DEFEATED ALL ALIENS`)
+    } else {
+      return alert(`SORRY, YOU LOSE!`)
+    }
+
+    const response = prompt(`Enter 'ok' to attack or 'r' to retreat`)
+    if (response.toLowerCase() === 'r') {
+      alert(`YOU LOSE!`)
+      defender.hull = 0;
+      defender.firepower = 0;
+      defender.accuracy = 0;
+      status()
+    } else if (response.toLowerCase() === 'ok') {
+      engage()
+    } else {
+      alert(`Try again!`)
+    }
+  }
+}
+// Battle
+const engage = () => {
+  defender.attack();
+  invader.attack();
+  lockAndLoad();
+}
+start(); 
